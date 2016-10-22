986526d6094978fa2faefaa061e3ab67003aab28
Adam Bowles <adam.bowles@nimvelo.com>
2016-10-22 15:03:07 +0100

    Better footer

--- src/components/Footer/component.jsx
+++ src/components/Footer/component.jsx
@@ -11,14 +11,16 @@ export default class TopBar extends Component {
   render() {
     return (
       <footer>
-        <FontAwesome name='tint' />
-        <ul>
-          <li>David Maitland</li>
-          <li>Josh Farrant</li>
-          <li>Dan Katri</li>
-          <li>Adam Bowles</li>
-        </ul>
-        <h2 className='title'>{strings.appName}</h2>
+        <div className='made-by'>
+          <p>Made with <FontAwesome name='heart-o' /> by:</p>
+          <ul className='makers'>
+            <li>David Maitland</li>
+            <li>Josh Farrant</li>
+            <li>Dan Katri</li>
+            <li>Adam Bowles</li>
+          </ul>
+        </div>
+        <p className='copyright'><FontAwesome name='copyright' /> 4 idiots</p>
       </footer>
     );
   }
